7a764c96575d9c8d4adb9b1e7fe793236e8c05af
Artyom <itcoding76@gmail.com>
2022-12-15 21:33:00 +0200

    add Seminar 2

---
+++ Seminar/Seminar2/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Задайте массив из 12 элементов, заполненный случайными числами
+из промежутка [-9, 9]. Найдите сумму отрицательных и положительных
+ элементов массива.
+Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] 
+сумма положительных чисел равна 29, сумма отрицательных равна -20.
+*/
+
+int create (int size, int minValue, int maxValue){
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue+1);
+        
+    }
+    return array;
+}
+
+void Showarray(int[] array){
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WrineLine();
+}
+
+int FindPosSumm(int[] array){
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+     if(array[i] > 0) summ += array[i];   
+    }
+    return summ;
+}
+
+int FindNegSumm(int[] array){
+    int summ = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+     if(array[i] < 0) summ += array[i];   
+    }
+    return summ;
+}
+
+int size = 0;
+int min = -9;
+int max = 9;
+int [] array = CreateArray(size, min, max);
+ShowArray(array);
+Console.WriteLine($"Summ of positive numbers is {FindPosSumm(array)}");
+Console.WriteLine($"Summ of negative numbers is {FindPosSumm(array)}");

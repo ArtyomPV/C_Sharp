b6c19ca25dac1e1416bbfa9b2478429fa8faa054
Artyom <itcoding76@gmail.com>
2022-12-16 06:37:39 +0200

    add Example 004 Div

---
+++ Lecture/Examples/Example003_Sum/Program.cs
@@ -0,0 +1,14 @@
+﻿// Console.Write("Input first number: ");
+// int numb1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Input second number: ");
+// int numb2 = Convert.ToInt32(Console.ReadLine());
+int numb1 = new Random().Next(1, 10);       //1 2 3 4 ... 9
+Console.WriteLine(numb1);
+
+int numb2 = new Random().Next(1, 10);       //1 2 3 4 ... 9
+Console.WriteLine(numb2);
+
+// int result = numb1 + numb2);
+// Console.WriteLine(result);
+
+Console.WriteLine(numb1 + numb2);
---
+++ Lecture/Examples/Example004_Div/Program.cs
@@ -0,0 +1,9 @@
+﻿int numberA = 10;
+int numberB = 5;
+
+Console.WriteLine(numberA/numberB);
+
+double number1 = 12;
+double number2 = 5;
+
+Console.WriteLine(number1/number2);
